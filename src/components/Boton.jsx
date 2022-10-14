431193a67f4eee7ec1aa5eb42948596800edee77
Diariela22 <102000928+Diariela22@users.noreply.github.com>
2022-10-13 21:01:38 -0400

    Adicion de url al componente boton

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -3,7 +3,7 @@ function Boton(props) {
         <div className={"mt-5 sm:mt-" + props.marginTop +" sm:flex-col sm:justify-" + props.justify + " lg:justify-end"}>
             <div className="rounded-full shadow shadow-gray-400">
                 <a
-                    href="#"
+                    href={props.url}
                     className={"flex w-" + props.width + " items-center justify-center rounded-full border border-transparent bg-" + props.color + "-400 py-1 px-5 text-sm font-medium text-" + props.textColor + " hover:bg-gray-400 md:py-1 md:px-4 md:text-sm"}
                 >
                     {props.title}
