7307c7549dab62a07ecb6a81f80f294df0ac8854
Diego Rodriguez <101908524+Haonter@users.noreply.github.com>
2022-10-29 00:43:25 -0300

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,35 +4,7 @@ import NavBarGuestRegister from '../components/NavBar/NavBarGuestRegister'
 import RegisterForm from '../components/Register';
 
 const Register = () => {
-    return ( 
-        <>
-            <NavBarGuestRegister/>
-            <form onSubmit={" --- Pendiente --- "}>
-                <RegisterForm />
-                <div className='mt-5 sm:flex-col'>
-                    <div className="rounded-full flex justify-center">
-                        <button
-                            type='submit'
-                            className="flex w-32 justify-center rounded-full bg-green-400 py-1 px-5 text-sm font-medium text-white hover:bg-gray-400 md:py-1 md:px-4 md:text-sm"
-                        >
-                            Registrate
-                        </button>
-                    </div>
-                </div>
-            </form>
-        
-            {/*<button 
-                type="submit" className='bg-blue rounded text-white' >
-                    Registrar
-            </button>*/}
-=======
-//import { useState } from 'react';
-import Input from "../components/input";
-
-
-const Register = () => {
-    //const [datos, setDatos] = useState([])
-
+    
     return ( 
         <>
             <div className='bg-white w-screen'>
@@ -113,7 +85,6 @@ const Register = () => {
                     </div>
                 </form>
             </div>
->>>>>>> Stashed changes
         </>
     );
 }
